4ec2fb1adf880313a8fb078276466b7c127ea45a
ImTani <tanishkdevil@gmail.com>
2022-07-30 00:06:56 +0530

    adding all files

---
+++ ExcelMarkOrganizer.py
@@ -0,0 +1,35 @@
+import openpyxl as op
+import tkinter as tk
+import tkinter.filedialog as fd
+import os
+
+base = tk.Tk()
+base.withdraw()
+# base.geometry('640x480')
+base.title("Grade Viwer")
+
+currdir = os.getcwd()
+filePath = fd.askopenfilename(parent=base, initialdir=currdir,
+                              title='Please select an excel file.',
+                              filetypes=[
+                                ("Excel Files", ".xlsx")
+                              ])
+
+print(filePath)
+path = filePath
+# path = input("Copy and paste excel file name here.") + ".xlsx"
+# path = 'Test' + ".xlsx"
+
+subNum = int(input("Enter Subject Code : "))
+wb = op.load_workbook(path)
+sheet_obj = wb.active
+
+maxRow = sheet_obj.max_row
+maxCol = sheet_obj.max_column
+
+cell_obj = sheet_obj['A2':'C12']
+
+for cell1, cell2, cell3 in cell_obj:
+    if cell2.value == subNum:
+        cell3 = sheet_obj.cell(row=cell3.row + 1, column=cell3.column)
+        print(cell1.value, cell2.value, cell3.value)
