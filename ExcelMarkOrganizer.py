a4d989b85f9406c2dd747ed74e4413ec1434f1aa
ImTani <tanishkdevil@gmail.com>
2022-07-30 16:47:15 +0530

    added .gitignore and basic columns in new xl file

--- ExcelMarkOrganizer.py
+++ ExcelMarkOrganizer.py
@@ -27,18 +27,27 @@ subNum = int(input("Enter Subject Code : "))
 wb = op.load_workbook(path)
 sheet_obj = wb.active
 
+firstRow = sheet_obj.min_row
+firstCol = sheet_obj.min_column
 maxRow = sheet_obj.max_row
 maxCol = sheet_obj.max_column
 
-cell_obj = sheet_obj['A2': str(d[maxCol])+str(maxRow)]
+cell_obj = sheet_obj[str(d[firstCol]) + str(firstRow):
+                     str(d[maxCol]) + str(maxRow)]
 
 newWB = op.Workbook()
 ws = newWB.active
 ws.title = "Marks for Sub. Code " + str(subNum)
-newWB.save("Student Marks.xlsx")
 
+ws['A1'] = "Name"
+ws['B1'] = "Marks"
+ws['C1'] = "Grade"
+
+h = 2
 for cell1, cell2, cell3 in cell_obj:
     if cell2.value == subNum:
         cell2 = sheet_obj.cell(row=cell2.row + 1, column=cell2.column)
         cell3 = sheet_obj.cell(row=cell3.row + 1, column=cell3.column)
-        print(cell1.value, cell2.value, cell3.value)
+        ws.cell(row=h, column=1).value = cell1.value
+
+newWB.save("Student Marks.xlsx")
